2dc74f635242b457010a046388efbc16c546b517
Juliana Nocchi <juliana.nocchi@spendesk.com>
2023-12-20 17:05:18 +0000

    Add books data and functions to practice object and array destructuring

---
+++ 02-JS-review/script.js
@@ -0,0 +1,69 @@
+const data = [
+  {
+    id: 1,
+    title: "Dune",
+    publicationDate: "1965-01-01",
+    author: "Frank Herbert",
+    genres: ["science fiction", "novel", "adventure"],
+    hasMovieAdaptation: true,
+    pages: 658,
+    translations: {
+      spanish: "",
+    },
+  },
+  {
+    id: 2,
+    title: "Harry Potter and the Philosopher's Stone",
+    publicationDate: "1997-06-26",
+    author: "J. K. Rowling",
+    genres: ["fantasy", "fantasy fiction", "adventure"],
+    hasMovieAdaptation: true,
+    pages: 223,
+    translations: {
+      spanish: "Harry Potter y la piedra filosofal",
+      korean: "해리 포터와 마법사의 돌",
+      bengali: "হ্যারি পটার এন্ড দ্য ফিলোসফার্স স্টোন",
+      portuguese: "Harry Potter e a Pedra Filosofal",
+    },
+  },
+  {
+    id: 3,
+    title: "A Game of Thrones",
+    publicationDate: "1996-08-01",
+    author: "George R. R. Martin",
+    genres: ["fantasy", "high-fantasy", "novel", "fantasy fiction"],
+    hasMovieAdaptation: true,
+    pages: 835,
+    translations: {
+      korean: "왕좌의 게임",
+      polish: "Gra o tron",
+      portuguese: "A Guerra dos Tronos",
+      spanish: "Juego de tronos",
+    },
+  },
+];
+
+function getBooks() {
+  return data;
+}
+
+function getBook(id) {
+  return data.find((d) => d.id === id);
+}
+
+const books = getBooks();
+books;
+
+const book2 = getBook(2);
+book2;
+
+const book = getBook(2);
+book; //returns book 2
+
+// Destructuring objects
+const { title, author, genres } = book;
+
+// Destructuring arrays
+const [firstGenre, , thirdGenre] = genres;
+
+console.log(title, author, firstGenre, thirdGenre);
