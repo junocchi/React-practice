136bd7b69c9b3737ccc0a8bcc48a0e712e498a33
Juliana Nocchi <juliana.nocchi@spendesk.com>
2023-12-21 11:21:43 +0000

    Add the rest element ...otherGenres

--- 02-JS-review/script.js
+++ 02-JS-review/script.js
@@ -1,22 +1,90 @@
 const data = [
   {
     id: 1,
-    title: "Dune",
-    publicationDate: "1965-01-01",
-    author: "Frank Herbert",
-    genres: ["science fiction", "novel", "adventure"],
+    title: "The Lord of the Rings",
+    publicationDate: "1954-07-29",
+    author: "J. R. R. Tolkien",
+    genres: [
+      "fantasy",
+      "high-fantasy",
+      "adventure",
+      "fiction",
+      "novels",
+      "literature",
+    ],
     hasMovieAdaptation: true,
-    pages: 658,
+    pages: 1216,
     translations: {
-      spanish: "",
+      spanish: "El señor de los anillos",
+      chinese: "魔戒",
+      french: "Le Seigneur des anneaux",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.52,
+        ratingsCount: 630994,
+        reviewsCount: 13417,
+      },
+      librarything: {
+        rating: 4.53,
+        ratingsCount: 47166,
+        reviewsCount: 452,
+      },
     },
   },
   {
     id: 2,
+    title: "The Cyberiad",
+    publicationDate: "1965-01-01",
+    author: "Stanislaw Lem",
+    genres: [
+      "science fiction",
+      "humor",
+      "speculative fiction",
+      "short stories",
+      "fantasy",
+    ],
+    hasMovieAdaptation: false,
+    pages: 295,
+    translations: {},
+    reviews: {
+      goodreads: {
+        rating: 4.16,
+        ratingsCount: 11663,
+        reviewsCount: 812,
+      },
+      librarything: {
+        rating: 4.13,
+        ratingsCount: 2434,
+        reviewsCount: 0,
+      },
+    },
+  },
+  {
+    id: 3,
+    title: "Dune",
+    publicationDate: "1965-01-01",
+    author: "Frank Herbert",
+    genres: ["science fiction", "novel", "adventure"],
+    hasMovieAdaptation: false,
+    pages: 658,
+    translations: {
+      spanish: "",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.25,
+        ratingsCount: 1142893,
+        reviewsCount: 49701,
+      },
+    },
+  },
+  {
+    id: 4,
     title: "Harry Potter and the Philosopher's Stone",
     publicationDate: "1997-06-26",
     author: "J. K. Rowling",
-    genres: ["fantasy", "fantasy fiction", "adventure"],
+    genres: ["fantasy", "adventure"],
     hasMovieAdaptation: true,
     pages: 223,
     translations: {
@@ -25,9 +93,21 @@ const data = [
       bengali: "হ্যারি পটার এন্ড দ্য ফিলোসফার্স স্টোন",
       portuguese: "Harry Potter e a Pedra Filosofal",
     },
+    reviews: {
+      goodreads: {
+        rating: 4.47,
+        ratingsCount: 8910059,
+        reviewsCount: 140625,
+      },
+      librarything: {
+        rating: 4.29,
+        ratingsCount: 120941,
+        reviewsCount: 1960,
+      },
+    },
   },
   {
-    id: 3,
+    id: 5,
     title: "A Game of Thrones",
     publicationDate: "1996-08-01",
     author: "George R. R. Martin",
@@ -39,6 +119,18 @@ const data = [
       polish: "Gra o tron",
       portuguese: "A Guerra dos Tronos",
       spanish: "Juego de tronos",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.44,
+        ratingsCount: 2295233,
+        reviewsCount: 59058,
+      },
+      librarything: {
+        rating: 4.36,
+        ratingsCount: 38358,
+        reviewsCount: 1095,
+      },
     },
   },
 ];
@@ -64,6 +156,11 @@ book; //returns book 2
 const { title, author, genres } = book;
 
 // Destructuring arrays
-const [firstGenre, , thirdGenre] = genres;
+//const [firstGenre, secondGenre] = genres;
 
-console.log(title, author, firstGenre, thirdGenre);
+// Rest element (...)
+const [firstGenre, secondGenre, ...otherGenres] = genres;
+console.log(title, author, firstGenre, secondGenre, otherGenres);
+// ['speculative fiction', 'short stories', 'fantasy']
+
+console.log(otherGenres);
