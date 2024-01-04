1e0f3f8b4eb5539d2041e4a5bf70b4c5a11440a4
Juliana Nocchi <juliana.nocchi@spendesk.com>
2024-01-04 10:30:37 +0000

    Add CSS classNames

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const pizzaData = [
   {
@@ -48,20 +49,51 @@ const pizzaData = [
 
 function App() {
   return (
-    <div>
-      <h1>Hello React!</h1>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
     </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our menu</h2>
+      <Pizza />
+      <Pizza />
+    </main>
+  );
+}
+
+function Footer() {
+  const hourNow = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hourNow >= openHour && hourNow <= closeHour;
+  console.log(isOpen);
+  return (
+    <footer className="footer">
+      We're open from 12:00 to 22:00. Come visit us or order online.<br></br>
+      The ⏰ time now is {new Date().toLocaleTimeString()}.
+    </footer>
   );
 }
 
 function Pizza() {
   return (
-    <div>
+    <div className="pizza">
       <img src="pizzas/spinaci.jpg" alt="spinach pizza" />
-      <h2>Pizza Spinaci</h2>
+      <h3>Pizza Spinaci</h3>
       <p>Tomato, mozarella, spinach, and ricotta cheese</p>
     </div>
   );
