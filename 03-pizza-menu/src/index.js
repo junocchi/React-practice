0ac9953745139b50e39cdf1e77025962429b3435
Juliana Nocchi <juliana.nocchi@spendesk.com>
2024-02-08 19:41:00 +0000

    Update tab name

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -96,12 +96,13 @@ function Pizza({ pizzaObj }) {
 
   // if (pizzaObj.soldOut) return null;
   return (
-    <li className="pizza">
+    // ternary operator is used in className to grayscale the sold out pizzas
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.soldOut ? "Sold out" : pizzaObj.price}</span>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
     </li>
   );
