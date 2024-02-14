a29e7da36ee18756f6740907671d63ccf2c19f1f
Juliana Nocchi <juliana.nocchi@spendesk.com>
2024-02-14 17:59:54 +0000

    Complete the date counter challenge

--- 04.1-date-counter/src/App.js
+++ 04.1-date-counter/src/App.js
@@ -8,55 +8,43 @@ export default function App() {
   );
 }
 
-function Steps() {
-  const [step, setStep] = useState(0);
-
-  function handlePreviousStep() {
-    setStep((currentStep) => currentStep - 1);
-  }
-  function handleNextStep() {
-    setStep((currentStep) => currentStep + 1);
-  }
-
-  return (
-    <div>
-      <p>Date Counter Challenge</p>
-      <p>
-        <button onClick={handlePreviousStep}>-</button>
-        Step: {step}
-        <button onClick={handleNextStep}>+</button>
-      </p>
-    </div>
-  );
-}
-
-function Count() {
-  const [count, setCount] = useState(0);
-
-  function handlePreviousCount() {
-    setCount((currentCount) => currentCount - 1);
-  }
-  function handleNextCount() {
-    setCount((currentCount) => currentCount + 1);
-  }
-
-  return (
-    <div>
-      <p>
-        <button onClick={handlePreviousCount}>-</button>
-        Count: {count}
-        <button onClick={handleNextCount}>+</button>
-      </p>
-    </div>
-  );
-}
-
 function Counter() {
+  const [count, setCount] = useState(0);
+  const [jump, setJump] = useState(1);
+  const date = new Date("february 14 2024");
+  date.setDate(date.getDate() + count);
+
   return (
     <div>
-      <Steps />
-      <Count />
-      <p> ? days from today is ?Date?</p>
+      <div>
+        <p>Date Counter Challenge</p>
+        <button onClick={() => setJump((currentJump) => currentJump - 1)}>
+          -
+        </button>
+        <span> Jump: {jump} </span>
+        <button onClick={() => setJump((currentJump) => currentJump + 1)}>
+          +
+        </button>
+      </div>
+      <div>
+        <button onClick={() => setCount((currentCount) => currentCount - jump)}>
+          -
+        </button>
+        <span> Count: {count} </span>
+        <button onClick={() => setCount((currentCount) => currentCount + jump)}>
+          +
+        </button>
+        <p>
+          <span>
+            {count === 0
+              ? "Today is "
+              : count > 0
+              ? `${count} days from today is `
+              : `${Math.abs(count)} days ago was `}
+          </span>
+          <span> {date.toDateString()} 🎉</span>
+        </p>
+      </div>
     </div>
   );
 }
