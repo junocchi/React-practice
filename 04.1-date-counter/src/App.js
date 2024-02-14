38cb3c095e1c4bf30c107bf89fd7023d93b74bf1
Juliana Nocchi <juliana.nocchi@spendesk.com>
2024-02-14 16:46:48 +0000

    handle buttons

--- 04.1-date-counter/src/App.js
+++ 04.1-date-counter/src/App.js
@@ -3,42 +3,60 @@ import { useState } from "react";
 export default function App() {
   return (
     <div className="App">
-      <Steps />
       <Counter />
     </div>
   );
 }
+
 function Steps() {
   const [step, setStep] = useState(0);
 
-  function handleNextStep() {
-    setStep((currentStep) => currentStep + 1);
-  }
   function handlePreviousStep() {
     setStep((currentStep) => currentStep - 1);
+  }
+  function handleNextStep() {
+    setStep((currentStep) => currentStep + 1);
   }
 
   return (
     <div>
       <p>Date Counter Challenge</p>
-      <p>Step: {step}</p>
+      <p>
+        <button onClick={handlePreviousStep}>-</button>
+        Step: {step}
+        <button onClick={handleNextStep}>+</button>
+      </p>
+    </div>
+  );
+}
+
+function Count() {
+  const [count, setCount] = useState(0);
+
+  function handlePreviousCount() {
+    setCount((currentCount) => currentCount - 1);
+  }
+  function handleNextCount() {
+    setCount((currentCount) => currentCount + 1);
+  }
+
+  return (
+    <div>
+      <p>
+        <button onClick={handlePreviousCount}>-</button>
+        Count: {count}
+        <button onClick={handleNextCount}>+</button>
+      </p>
     </div>
   );
 }
 
 function Counter() {
-  const [count, setCount] = useState(0);
-
-  function handleNextCount() {
-    setCount((currentCount) => currentCount + 1);
-  }
-  function handlePreviousCount() {
-    setCount((currentCount) => currentCount - 1);
-  }
-
   return (
     <div>
-      <p>Count:{count}</p>
+      <Steps />
+      <Count />
+      <p> ? days from today is ?Date?</p>
     </div>
   );
 }
