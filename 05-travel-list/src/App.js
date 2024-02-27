2cdf0683f73d819ad1110dde1641d22fa2fa7872
Juliana Nocchi <juliana.nocchi@spendesk.com>
2024-02-27 15:54:43 +0000

    Avoid page reloading when submitting the form

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -2,7 +2,11 @@ import { useState } from "react";
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+  { id: 4, description: "T-shirts", quantity: 8, packed: false },
+  { id: 5, description: "Sunglasses", quantity: 1, packed: false },
+  { id: 6, description: "Shampoo", quantity: 1, packed: false },
 ];
 
 export default function App() {
@@ -21,22 +25,55 @@ function Logo() {
 }
 
 function Form() {
+  function handleSubmit(e) {
+    // we use this to avoid page reloading when the user presses the button to submit the form
+    e.preventDefault();
+  }
+
   return (
-    <div className="add-form">
+    <form
+      className="add-form"
+      onSubmit={
+        handleSubmit /* we don't call the function here. Instead React, will call the function for us as soon as the onSubmit event happens. */
+      }
+    >
       <h3>What do you need for your 😍 trip?</h3>
+      <select>
+        {Array.from({ length: 20 }, (value, index) => index + 1).map((num) => (
+          <option key={num} value={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="e.g. t-shirt"></input>
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList() {
+  const items = initialItems;
+  return (
+    <div className="list">
+      <ul>
+        {items.map((item) => (
+          <Item item={item} key={item.id} />
+          // when we get an array of objects, each of these objects should have a key
+        ))}
+      </ul>
     </div>
   );
 }
 
-function Item() {
-  return <div className="list"></div>;
-}
-
-function PackingList() {
+function Item({ item }) {
+  // destructured form of Item(props)
   return (
-    <div className="list">
-      <Item />
-    </div>
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.description} {item.quantity}
+      </span>
+      <button>❌</button>
+    </li>
   );
 }
 
